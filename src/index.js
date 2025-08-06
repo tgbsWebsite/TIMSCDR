c4d165d903158a46e1c60a0308ae51ac29c0ad58
tgbsdigital <tgbsdigital@gmail.com>
2025-08-06 14:07:45 +0530

    Footer and navbar button

--- src/index.js
+++ src/index.js
@@ -28,12 +28,10 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/VisionMission" element={<VisionMissionPage />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
 );
